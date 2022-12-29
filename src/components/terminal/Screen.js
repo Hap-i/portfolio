5920b8fccd44d50d8cde64155a7a9f2c003dc958
suvendu sekhar sahoo <suvendusekharsahoo5@gmail.com>
2022-12-29 20:38:38 +0530

    added auto foucos on scroll and run command

--- src/components/terminal/Screen.js
+++ src/components/terminal/Screen.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
-import { helpData, lsHome, lsProjects } from "../../utils/terminalData/data";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  aboutData,
+  helpData,
+  lsHome,
+  lsProjects,
+} from "../../utils/terminalData/data";
 
 function Screen() {
   const [terminalHistory, setterminalHistory] = useState(
     React.createElement("div", { children: [] })
   );
   const [cwd, setcwd] = useState("");
+
+  const commandLine = useRef();
 
   function handleTerminalHistory(components) {
     let temp = React.createElement("div", {
@@ -24,7 +31,7 @@ function Screen() {
     });
   }
 
-  function formatHelpData(helpData) {
+  function formatHelpData() {
     let commandComponents = helpData.map((data, i) => {
       return (
         <div key={i} className="text-green-500">
@@ -55,7 +62,7 @@ function Screen() {
       children: [commandDiv, descriptionDiv],
     });
 
-    return addCommandLine("help", helpDataDiv);
+    handleTerminalHistory(addCommandLine("help", helpDataDiv));
   }
 
   function handlels() {
@@ -78,10 +85,10 @@ function Screen() {
       });
     }
     let lsDiv = React.createElement("div", {
-      className: "grid grid-cols-4 w-4/5",
+      className: "grid grid-cols-4 w-3/5",
       children: lscomponents,
     });
-    return addCommandLine("ls", lsDiv);
+    handleTerminalHistory(addCommandLine("ls", lsDiv));
   }
 
   function changeCwd(dir, command) {
@@ -95,11 +102,27 @@ function Screen() {
     setterminalHistory(React.createElement("div", { children: [] }));
   }
 
+  function handleAbout() {
+    const ptag = React.createElement("p", {
+      className: "text-white w-3/5",
+      children: aboutData,
+    });
+
+    handleTerminalHistory(
+      addCommandLine(
+        "run about.md",
+        React.createElement("div", {
+          children: ptag,
+        })
+      )
+    );
+  }
+
   function handleCommandResponse(command) {
     if (command === "help") {
-      handleTerminalHistory(formatHelpData(helpData));
+      formatHelpData();
     } else if (command === "ls") {
-      handleTerminalHistory(handlels());
+      handlels();
     } else if (command === "cd projects" || command === "cd projects/") {
       changeCwd("projects/", "cd projects/");
     } else if (command === "cd ~") {
@@ -108,12 +131,16 @@ function Screen() {
       clearTerminal();
     } else if (command === "cd ../") {
       changeCwd("", "cd ../");
-    } else if (command === "run about.md") {
-    } else if (command === "run contact.md") {
-    } else if (command === "run education.md") {
-    } else if (command === "run experience.md") {
-    } else if (command === "run achivements.md") {
-    } else if (command === "run skills.md") {
+    } else if (command.includes("run")) {
+      let fileName = command.trim().split(" ")[1];
+      if (cwd === "" && fileName === "about.md") handleAbout();
+      else if (cwd === "" && fileName === "achivements.md") handleAbout();
+      else if (cwd === "" && fileName === "contact.md") handleAbout();
+      else if (cwd === "" && fileName === "education.md") handleAbout();
+      else if (cwd === "" && fileName === "experience.md") handleAbout();
+      else if (cwd === "" && fileName === "skills.md") handleAbout();
+      else {
+      }
     } else {
     }
   }
@@ -123,36 +150,44 @@ function Screen() {
       e.target.value = "";
     }
   }
+
+  useEffect(() => {
+    commandLine.current.scrollIntoView();
+  });
+
   return (
     <>
       <div className="flex justify-between">
-        <div className="bg-slate-800 h-screen w-screen pl-4">
+        <div className="bg-slate-800 h-screen w-screen pl-4 pb-20 overflow-auto">
           <div className="animate-type"></div>
           <div className="text-white" id="history">
             {terminalHistory}
           </div>
           <div className="flex justify-start">
-            <div className="text-white">{`guest@hapi.com : ${cwd} $`}</div>
+            <div
+              ref={commandLine}
+              className="text-white">{`guest@hapi.com : ${cwd} $`}</div>
             <input
-              className="pl-2 bg-slate-800 border-none w-auto text-white caret-white caret-2 focus: outline-none"
+              className="pl-2 bg-slate-800 border-none w-3/5 text-white caret-white caret-2 focus: outline-none"
               type="text"
               onKeyUp={handleKeyUp}
             />
           </div>
-          {/* <div className="my-4">
-            <div className="text-white mb-4">guest@hapi.com :~$ ls</div>
-            <div className="text-green-500 grid grid-cols-4 w-4/5">
-              <div>about.md</div>
-              <div>achivements.md</div>
-              <div>contact.md</div>
-              <div>education.md</div>
-              <div>experience.md</div>
-              <div className="text-blue-800">projects/</div>
-              <div>skills.md</div>
-            </div>
+          {/* <div>
+            <p className="text-white w-3/5">
+              Experienced software engineer with a passion for developing
+              innovative programs that expedite the efficiency and effectiveness
+              of organizational success. Well-versed in technology and writing
+              code to create systems that are reliable and user-friendly.
+              Skilled leader who has the proven ability to motivate, educate,
+              and manage a team of professionals to build software programs and
+              effectively track changes. Confident communicator, strategic
+              thinker, and innovative creator to develop software that is
+              customized to meet a company’s organizational needs, highlight
+              their core competencies, and further their success
+            </p>
           </div> */}
         </div>
-        <div className="w-4/5 ">output</div>
       </div>
     </>
   );
--- src/utils/terminalData/data.js
+++ src/utils/terminalData/data.js
@@ -56,10 +56,27 @@ const lsHome = [
   },
 ];
 
-const lsProjects = ["project1.md", "project2.md", "project3.md"];
+const lsProjects = [
+  "allProjects.md",
+  "project1.md",
+  "project2.md",
+  "project3.md",
+];
+
+const aboutData = `Experienced software engineer with a passion for developing
+innovative programs that expedite the efficiency and effectiveness
+of organizational success. Well-versed in technology and writing
+code to create systems that are reliable and user-friendly.
+Skilled leader who has the proven ability to motivate, educate,
+and manage a team of professionals to build software programs and
+effectively track changes. Confident communicator, strategic
+thinker, and innovative creator to develop software that is
+customized to meet a company’s organizational needs, highlight
+their core competencies, and further their success`;
 
 module.exports = {
   helpData,
   lsHome,
   lsProjects,
+  aboutData,
 };
