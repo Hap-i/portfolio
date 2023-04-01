9353027dacf0c14eba17001654bea0edc8de0fa9
suvendu sekhar sahoo <suvendusekharsahoo5@gmail.com>
2023-04-01 23:37:42 +0530

    All projects page template created

---
+++ src/components/visual/ProjectComponents/AllProjectCards.js
@@ -0,0 +1,149 @@
+import React from "react";
+
+function AllProjectCards() {
+  return (
+    <div className="mt-16">
+      <div className="flex justify-center">
+        <div className="grid lg:grid-cols-3 gap-y-7 gap-x-8 sm:grid-cols-1 md:grid-cols-2 mx-10">
+          <div className="max-w-sm bg-white border border-gray-300 rounded-lg shadow-2xl">
+            <img
+              className="rounded-t-lg"
+              src="assets/projects/sorting-visualizer.png"
+              alt=""
+            />
+            <div className="pt-2 pb-10">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 text-center">
+                Sorting Visualizer
+              </h5>
+              <div className="text-center">
+                Sorting Algorithm visualizer tool
+              </div>
+              <div className="text-white space-x-5 text-center mt-3">
+                <button
+                  className="text-white bg-black py-1 rounded-lg w-32  text-xl hover:text-black hover:bg-transparent
+          border border-black">
+                  Github
+                </button>
+                <button
+                  className="text-white bg-black py-1 rounded-lg  w-32 text-xl hover:text-black hover:bg-transparent
+          border border-black">
+                  Live
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-sm bg-white border border-gray-300 rounded-lg shadow-2xl">
+            <img
+              className="rounded-t-lg"
+              src="assets/projects/sorting-visualizer.png"
+              alt=""
+            />
+            <div className="pt-2 pb-10">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 text-center">
+                Sorting Visualizer
+              </h5>
+              <div className="text-center">
+                Sorting Algorithm visualizer tool
+              </div>
+              <div className="text-white space-x-5 text-center mt-3">
+                <button
+                  className="text-white bg-black py-1 rounded-lg w-32  text-xl hover:text-black hover:bg-transparent
+          border border-black">
+                  Github
+                </button>
+                <button
+                  className="text-white bg-black py-1 rounded-lg  w-32 text-xl hover:text-black hover:bg-transparent
+          border border-black">
+                  Live
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-sm bg-white border border-gray-300 rounded-lg shadow-2xl">
+            <img
+              className="rounded-t-lg"
+              src="assets/projects/sorting-visualizer.png"
+              alt=""
+            />
+            <div className="pt-2 pb-10">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 text-center">
+                Sorting Visualizer
+              </h5>
+              <div className="text-center">
+                Sorting Algorithm visualizer tool
+              </div>
+              <div className="text-white space-x-5 text-center mt-3">
+                <button
+                  className="text-white bg-black py-1 rounded-lg w-32  text-xl hover:text-black hover:bg-transparent
+          border border-black">
+                  Github
+                </button>
+                <button
+                  className="text-white bg-black py-1 rounded-lg  w-32 text-xl hover:text-black hover:bg-transparent
+          border border-black">
+                  Live
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-sm bg-white border border-gray-300 rounded-lg shadow-2xl">
+            <img
+              className="rounded-t-lg"
+              src="assets/projects/sorting-visualizer.png"
+              alt=""
+            />
+            <div className="pt-2 pb-10">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 text-center">
+                Sorting Visualizer
+              </h5>
+              <div className="text-center">
+                Sorting Algorithm visualizer tool
+              </div>
+              <div className="text-white space-x-5 text-center mt-3">
+                <button
+                  className="text-white bg-black py-1 rounded-lg w-32  text-xl hover:text-black hover:bg-transparent
+          border border-black">
+                  Github
+                </button>
+                <button
+                  className="text-white bg-black py-1 rounded-lg  w-32 text-xl hover:text-black hover:bg-transparent
+          border border-black">
+                  Live
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-sm bg-white border border-gray-300 rounded-lg shadow-2xl">
+            <img
+              className="rounded-t-lg"
+              src="assets/projects/sorting-visualizer.png"
+              alt=""
+            />
+            <div className="pt-2 pb-10">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 text-center">
+                Sorting Visualizer
+              </h5>
+              <div className="text-center">
+                Sorting Algorithm visualizer tool
+              </div>
+              <div className="text-white space-x-5 text-center mt-3">
+                <button
+                  className="text-white bg-black py-1 rounded-lg w-32  text-xl hover:text-black hover:bg-transparent
+          border border-black">
+                  Github
+                </button>
+                <button
+                  className="text-white bg-black py-1 rounded-lg  w-32 text-xl hover:text-black hover:bg-transparent
+          border border-black">
+                  Live
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AllProjectCards;
