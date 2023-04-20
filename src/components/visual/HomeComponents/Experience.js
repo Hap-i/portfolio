c3b5716d72302c7330f874bfdd57a57b7dddd9a9
suvendu sekhar sahoo <suvendusekharsahoo5@gmail.com>
2023-04-21 01:02:43 +0530

    Responsive fixed for mobile

--- src/components/visual/HomeComponents/Experience.js
+++ src/components/visual/HomeComponents/Experience.js
@@ -1,59 +1,61 @@
 import React from "react";
 
 function Experience() {
+  const experience = [
+    {
+      name: "Tata Consultancy Services",
+      designation: "Systems Engineer",
+      duration: "July 2021 - Present",
+      responsiblities: [
+        `Implemented a Ticketing system for a chatbot to handle customers
+        coming in out-of-office hours, resulting in 34% more customer
+        engagement.`,
+        `Worked on integrating various third-party systems with chatbot
+        such as CRM, RPA bots, SendGrid, Google Cloud Functions, and
+        Storage.`,
+        `Contributed majorly to Anomaly Detection, custom widgets,
+        webform, and logging features.`,
+        `Developed Custom Widgets for Agents`,
+      ],
+    },
+    {
+      name: "Target Integration",
+      designation: "Odoo Developer",
+      duration: "Oct 2020 - Jan 2021",
+      responsiblities: [
+        `Got To learn about ERP systems`,
+        `Worked on a Hospital Management ERP system using Odoo`,
+      ],
+    },
+  ];
   return (
-    <div className="px-72">
-      <div className="flex justify-center mt-28 text-4xl font-semibold mb-12">
+    <div className="md:px-72 px-5">
+      <div className="flex justify-center mt-28 text-3xl md:text-4xl font-semibold mb-12 ">
         Work Experience
       </div>
       <div className="flex justify-center">
         <ol className="relative border-l border-gray-200 dark:border-gray-700">
-          <li className="mb-10 ml-4">
-            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              July 2021 - Present
-            </time>
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-black">
-              Systems Engineer
-            </h3>
-            <h3 className="text-xl font-semibold text-gray-900 dark:text-black">
-              Tata Consultancy Services
-            </h3>
-            {/* <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"> */}
-            <ul className="list-outside text-black">
-              <li>
-                Implemented a Ticketing system for a chatbot to handle customers
-                coming in out-of-office hours, resulting in 34% more customer
-                engagement.
+          {experience.map((exp, i) => {
+            return (
+              <li className="mb-10 ml-4 " key={i}>
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  {exp.duration}
+                </time>
+                <h3 className="text-lg font-semibold text-gray-900 dark:text-black">
+                  {exp.designation}
+                </h3>
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-black">
+                  {exp.name}
+                </h3>
+                <ol className="list-disc text-black pl-2">
+                  {exp.responsiblities.map((responsibility, j) => {
+                    return <li key={j}>{responsibility}</li>;
+                  })}
+                </ol>
               </li>
-              <li>
-                Worked on integrating various third-party systems with chatbot
-                such as CRM, RPA bots, SendGrid, Google Cloud Functions, and
-                Storage.
-              </li>
-              <li>
-                Contributed majorly to Anomaly Detection, custom widgets,
-                webform, and logging features.
-              </li>
-              <li>Developed Custom Widgets for Agents</li>
-            </ul>
-            {/* </p> */}
-          </li>
-          <li className="mb-10 ml-4">
-            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              Oct 2020 - Jan 2021
-            </time>
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-black">
-              Target Integration
-            </h3>
-            {/* <p className="mb-4 text-base font-normal dark:text-gray-400"> */}
-            <ul className="list-outside text-black">
-              <li>Got To learn about ERP systems</li>
-              <li>Worked on a Hospital Management ERP system using Odoo</li>
-            </ul>
-            {/* </p> */}
-          </li>
+            );
+          })}
         </ol>
       </div>
     </div>
--- src/components/visual/HomeComponents/FeaturedProjects.js
+++ src/components/visual/HomeComponents/FeaturedProjects.js
@@ -4,34 +4,55 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 function FeaturedProjects() {
+  const proj1Feat = ["Authen", "Auhkl", "djlk k", "sdfsdf"];
+  const proj2Feat = ["Authen", "Auhkl", "djlk k", "sdfsdf"];
   return (
     <div className="mt-20 md:mt-36 ">
       <div className="flex flex-col items-center">
-        <div className="text-5xl font-semibold mb-28">Featured Projects</div>
+        <div className="text-3xl md:text-5xl font-semibold mb-16 md:mb-28">
+          Featured Projects
+        </div>
       </div>
-      <div className="flex justify-center space-x-20 mb-40">
-        <div className="h-1/5 w-2/5">
+      <div className="flex flex-col md:flex-row justify-center items-center md:space-x-20 md:mb-32  mb-16">
+        <div className="md:w-[500px] w-[300px]">
           <img src="assets/projects/sorting-visualizer.png" alt="" />
         </div>
         <div className="flex flex-col justify-center">
-          <div className="text-3xl mb-10">Sorting Visualizer</div>
-          <div className="grid grid-cols-2 gap-2">
-            <div>Merge Sort</div>
-            <div>Selection Sort</div>
-            <div>Bubble Sort</div>
+          <div className="text-xl md:text-3xl md:mb-10 mb-5 md:mt-0 mt-5">
+            Sorting Visualizer
+          </div>
+          <div className="grid grid-cols-2 gap-x-4 gap-y-2">
+            {proj1Feat.map((e) => {
+              return (
+                <div className="flex items-center space-x-1" key={e}>
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="3"
+                    className="text-green-500 w-4 h-4  md:w-6 md:h-6 flex-shrink-0"
+                    viewBox="0 0 24 24">
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </svg>
+                  <div>{e}</div>
+                </div>
+              );
+            })}
           </div>
           <div
             className="
-          flex space-x-5">
+          flex md:space-x-5 space-x-3">
             <button
-              className="text-white bg-black mt-5 px-3 py-2 rounded-lg w-44
+              className="text-white bg-black mt-5 px-3 py-2 rounded-lg md:w-44 w-28
             hover:scale-105 duration-2000 
           hover:bg-white hover:text-black hover:border 
             border-2 border-black">
               Live
             </button>
             <button
-              className="text-white bg-black mt-5 px-3 py-2 rounded-lg w-44
+              className="text-white bg-black mt-5 px-3 py-2 rounded-lg md:w-44 w-28
             hover:scale-105 duration-2000 
           hover:bg-white hover:text-black hover:border 
             border-2 border-black">
@@ -40,41 +61,43 @@ function FeaturedProjects() {
           </div>
         </div>
       </div>
-      <div className="flex justify-center space-x-20">
+      <div className="flex flex-col-reverse md:flex-row justify-center items-center md:space-x-20 mb-16">
         <div className="flex flex-col justify-center">
-          <div className="text-3xl mb-10">Shinobi Docs</div>
+          <div className="text-xl md:text-3xl md:mb-10 mb-5 md:mt-0 mt-5">
+            Sorting Visualizer
+          </div>
           <div className="grid grid-cols-2 gap-2">
-            <div className="flex items-center space-x-2">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="3"
-                className="text-green-500 w-6 h-6 flex-shrink-0"
-                viewBox="0 0 24 24">
-                <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                <path d="M22 4L12 14.01l-3-3"></path>
-              </svg>
-              <div>Authentication</div>
-            </div>
-            <div>Rich Text Editor</div>
-            <div>Realtime Colab</div>
-            <div>Document Sharing</div>
-            <div>Realtime Data storing</div>
+            {proj2Feat.map((e) => {
+              return (
+                <div className="flex items-center space-x-1" key={e}>
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="3"
+                    className="text-green-500 w-4 h-4  md:w-6 md:h-6 flex-shrink-0"
+                    viewBox="0 0 24 24">
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </svg>
+                  <div>{e}</div>
+                </div>
+              );
+            })}
           </div>
           <div
             className="
-          flex space-x-5">
+          flex md:space-x-5 space-x-3">
             <button
-              className="text-white bg-black mt-5 px-3 py-2 rounded-lg w-44
+              className="text-white bg-black mt-5 px-3 py-2 rounded-lg md:w-44 w-28
             hover:scale-105 duration-2000 
           hover:bg-white hover:text-black hover:border 
             border-2 border-black">
               Live
             </button>
             <button
-              className="text-white bg-black mt-5 px-3 py-2 rounded-lg w-44
+              className="text-white bg-black mt-5 px-3 py-2 rounded-lg md:w-44 w-28
             hover:scale-105 duration-2000 
           hover:bg-white hover:text-black hover:border 
             border-2 border-black">
@@ -82,7 +105,7 @@ function FeaturedProjects() {
             </button>
           </div>
         </div>
-        <div className="h-1/5 w-2/5">
+        <div className="md:w-[500px] w-[300px]">
           <img src="assets/projects/sorting-visualizer.png" alt="" />
         </div>
       </div>
